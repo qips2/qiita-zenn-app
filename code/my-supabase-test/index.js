16e3728179f544d44aada5df0553a60a275251c9
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2026-01-12 19:23:53 +0900

    [add]supabaseの疎通確認

---
+++ code/my-supabase-test/index.js
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { supabase } from './supabaseClient.js';
+
+async function main() {
+  // テーブルからすべて取得（例: public.todos）
+  const { data, error } = await supabase.from('books').select('*');
+
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('Data:', data);
+  }
+
+  //Database Function経由
+  
+}
+
+main();
