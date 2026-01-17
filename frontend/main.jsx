79ad5631409ed6ff20f73873e80f158854e72f25
ma2ta9 <ma2ymta9m@icloud.com>
2026-01-17 15:24:52 +0900

    フロントエンドのテスト画面

--- frontend/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App.jsx'　　　テスト用のjsxファイルの呼び出し
-import VercelTest from './main/VercelTest'
-
-createRoot(document.getElementById('root')).render(
-  <VercelTest />   //VercelTestを呼び出す。
-)
